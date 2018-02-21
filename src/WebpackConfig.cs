575a88f1c0352c59863abf1d038ec324c8312e7b
Willem Bijker <willem@bijker.co.za>
2018-02-21 12:29:59 +0200

    Add different referencing modes

--- src/WebpackConfig.cs
+++ src/WebpackConfig.cs
@@ -2,6 +2,29 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreWebpackManifest
 {
+    /*
+    Usage has to do with where the a request webpack chunk lies:
+    -It can either be served as static content from prebuild webpack
+        <script src="dist/app.1230492kad4sd.js"/>
+    -or it can be served from webpack dev server.
+        <script src="http://localhost:8080/webpack/app.js"/>
+    */
+    public enum WebpackConfigUsage
+    {
+        // Autodetect will use build if buld files is available
+        // otherwise will use dev server
+        AUTODETECT,
+        
+        // IHostingEnvironment will be used to check if app runs as dev or build
+        ENVIRONMENT,
+
+        // Always use development server
+        DEVSERVER,
+
+        // Always use prebuild files
+        BUILD
+    }
+
     public class WebpackConfig
     {
         // Host, Port and public path is all dev server configurations
@@ -11,11 +34,10 @@ namespace CoreWebpackManifest
         public string PublicPath { get; set; }
         // Manifest is the name of the file produces from webpack Manifest plugin
         public string Manifest { get; set; }
-        
-        // Wheter or not to use dev server or build directory
-        public bool Development { get; set; }
 
-        // The directory where webpack will dump built files
+        public WebpackConfigUsage Usage { get; set; }
+
+        // The directory where webpack will output built files
         public string BuildDirectory { get; set; }
 
         public WebpackConfig()
@@ -24,7 +46,7 @@ namespace CoreWebpackManifest
             Port = 8080;
             PublicPath = "webpack";
             Manifest = "manifest.json";
-            Development = true;
+            Usage = WebpackConfigUsage.AUTODETECT;
             BuildDirectory = "build";
         }
 
